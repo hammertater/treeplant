217606f3b4a581ae7c9323f712e51a6fa976738a
hammertater <74752031+hammertater@users.noreply.github.com>
2023-10-22 22:08:06 -0400

    Fix planting on plowed dirt

--- shared/src/main/java/ht/treeplant/server/util/PlantUtil.java
+++ shared/src/main/java/ht/treeplant/server/util/PlantUtil.java
@@ -1,5 +1,6 @@
 package ht.treeplant.server.util;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -9,18 +10,20 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.Nullable;
 
 public class PlantUtil {
 
     public static boolean tryToPlant(ItemEntity entity) {
         try {
+            Vec3 pos = entity.position().add(0, 0.2, 0);
             InteractionResult result = entity.getItem().useOn(new UseOnContextAccessor(
                     entity.getLevel(),
                     null,
                     InteractionHand.MAIN_HAND,
                     entity.getItem(),
-                    new BlockHitResult(entity.position(), Direction.DOWN, entity.blockPosition(), true))
+                    new BlockHitResult(pos, Direction.DOWN, new BlockPos(pos), true))
             );
             return result == InteractionResult.SUCCESS;
         } catch (NullPointerException ignored) {
